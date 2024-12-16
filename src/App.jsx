4e6bbf390d0f7fc43564a1b88aed9e174687044c
Chayan <chayankoley5678@gmail.com>
2024-12-16 20:31:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+            <Route index element={<h1>hello world 2</h1>}></Route>
+            <Route path='movie/:id' element={<h1>Movie detail page</h1>}></Route>
+            <Route path='movies/:type' element={<h1>Movies list page</h1>}></Route>
+            <Route path='/*' element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
